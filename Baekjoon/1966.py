b810d9037d10750706b5756ecd65c4c17ff0eb87
yerimYeon <99879845+yinneu@users.noreply.github.com>
2022-08-18 07:59:00 +0900

    1966(수정)

--- Baekjoon/1966.py
+++ Baekjoon/1966.py
@@ -7,25 +7,28 @@ read = sys.stdin.readline
 testCase = int(read())
 
 for t in range(testCase):  
+    
     # 문서 개수와 궁금한 문서의 위치
     n, m = map(int,read().split())
     priority = list(map(int,read().split()))
-
-    queue = deque([])
+    
+    # 중요도와 처음 큐에서의 위치
+    queue = deque([])    
     for i in range(n):
         queue.append([priority[i], i])
         
     target = queue[m]
     printCount = 0
-
+    
     while queue:
         flag = False
         x = queue[0]      
         for i in range(1,len(queue)):
+            # 더 중요한 문서가 있을때  
             if x[0] < queue[i][0]:
                 flag = True
                 break
-                    
+                  
         if flag:
             queue.popleft()
             queue.append(x)
